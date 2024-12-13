56d6cffb76448cc27688b7b9df5b10db2d2493af
Daniele Zanaga <gugout@gmail.com>
2024-12-13 17:05:34 +0100

    Refactor composite.py for improved readability and consistency

--- src/satio_pc/preprocessing/composite.py
+++ src/satio_pc/preprocessing/composite.py
@@ -1,37 +1,40 @@
 import warnings
 from datetime import datetime, timedelta
-from dateutil.parser import parse as parse_date
 
-import pandas as pd
-import numpy as np
 import dask.array as da
+import numpy as np
+import pandas as pd
 import xarray as xr
+from dateutil.parser import parse as parse_date
 from loguru import logger
 
-
-SUPPORTED_MODES = ['median', 'mean', 'sum', 'min', 'max']
+SUPPORTED_MODES = ["median", "mean", "sum", "min", "max"]
 
 
 def nonzero_reducer(arr, mode):
     """Compute nanmedian on array. If array is not float32/float64,
     the array is converted to float and 0 values are skipped.
 
-    mode can be 'median', 'mean', 'sum' """
+    mode can be 'median', 'mean', 'sum'"""
 
     if isinstance(arr, da.core.Array):
         reducer_lib = da
     else:
         reducer_lib = np
 
-    reducer_func = {'median': reducer_lib.nanmedian,
-                    'mean': reducer_lib.nanmean,
-                    'sum': reducer_lib.nansum,
-                    'min': reducer_lib.nanmin,
-                    'max': reducer_lib.nanmax}.get(mode)
+    reducer_func = {
+        "median": reducer_lib.nanmedian,
+        "mean": reducer_lib.nanmean,
+        "sum": reducer_lib.nansum,
+        "min": reducer_lib.nanmin,
+        "max": reducer_lib.nanmax,
+    }.get(mode)
 
     if reducer_func is None:
-        raise ValueError(f"Compositing mode '{mode}' not supported. "
-                         f"Should be one of {SUPPORTED_MODES}.")
+        raise ValueError(
+            f"Compositing mode '{mode}' not supported. "
+            f"Should be one of {SUPPORTED_MODES}."
+        )
 
     start_dtype = arr.dtype
     if start_dtype not in (np.float32, np.float64):
@@ -50,25 +53,27 @@ def nonzero_reducer(arr, mode):
 
 
 def _get_date_range(start, end, freq, window):
-
     before, after = _get_before_after(window)
 
     start, end = parse_date(start), parse_date(end)
 
-    date_range = pd.date_range(start=start + timedelta(days=before),
-                               end=end + timedelta(days=-after),
-                               freq=f'{freq}D')
+    date_range = pd.date_range(
+        start=start + timedelta(days=before),
+        end=end + timedelta(days=-after),
+        freq=f"{freq}D",
+    )
     return date_range
 
 
-def calculate_moving_composite(darr: xr.DataArray,
-                               freq=7,
-                               window=None,
-                               start=None,
-                               end=None,
-                               mode='median',
-                               use_all_obs=True,
-                               ):
+def calculate_moving_composite(
+    darr: xr.DataArray,
+    freq=7,
+    window=None,
+    start=None,
+    end=None,
+    mode="median",
+    use_all_obs=True,
+):
     """
     Calculate moving median composite of an hyper cube with shape [bands, time,
     rows, cols]
@@ -102,22 +107,31 @@ def calculate_moving_composite(darr: xr.DataArray,
     Tuple of time_vector and composite 4d array
     """
     if mode not in SUPPORTED_MODES:
-        raise ValueError(('Compositing mode should be one of '
-                          f'{SUPPORTED_MODES}, but got: `{mode}`'))
+        raise ValueError(
+            (
+                "Compositing mode should be one of "
+                f"{SUPPORTED_MODES}, but got: `{mode}`"
+            )
+        )
 
-    no_ov_modes = ['sum', 'min', 'max']
+    no_ov_modes = ["sum", "min", "max"]
     if mode in no_ov_modes:
         if window is not None and window != freq:
-            logger.warning(('`window` argument is ignored for '
-                            f'compositing mode `{no_ov_modes}`.'))
+            logger.warning(
+                (
+                    "`window` argument is ignored for "
+                    f"compositing mode `{no_ov_modes}`."
+                )
+            )
         # For these modes window overlap is not allowed in the time subsets
         # to avoid double counting of values
         window = freq
     window = window or freq  # if window is None, default to `freq` days
 
     if window < freq:
-        raise ValueError('`window` value should be equal or greater than '
-                         '`freq` value.')
+        raise ValueError(
+            "`window` value should be equal or greater than " "`freq` value."
+        )
 
     time = darr.time.values
 
@@ -126,24 +140,21 @@ def calculate_moving_composite(darr: xr.DataArray,
 
     date_range = _get_date_range(start, end, freq, window)
 
-    comp_shape = (len(date_range), darr.shape[1],
-                  darr.shape[2], darr.shape[3])
+    comp_shape = (len(date_range), darr.shape[1], darr.shape[2], darr.shape[3])
 
     if isinstance(darr.data, da.core.Array):
-        comp = da.zeros(comp_shape,
-                        chunks=(1, 1, darr.shape[2], darr.shape[3]),
-                        dtype=darr.dtype)
+        comp = da.zeros(
+            comp_shape,
+            chunks=(1, 1, darr.shape[2], darr.shape[3]),
+            dtype=darr.dtype,
+        )
     else:
-        comp = np.zeros(comp_shape,
-                        dtype=darr.dtype)
+        comp = np.zeros(comp_shape, dtype=darr.dtype)
 
-    time = darr.time.values
     before, after = _get_before_after(window)
-    intervals_flags = _get_invervals_flags(date_range,
-                                           time,
-                                           before,
-                                           after,
-                                           use_all_obs)
+    intervals_flags = _get_invervals_flags(
+        date_range, time, before, after, use_all_obs
+    )
 
     for i, d in enumerate(date_range):
         flag = intervals_flags[i]
@@ -152,36 +163,32 @@ def calculate_moving_composite(darr: xr.DataArray,
         idxs = np.where(flag)[0]
         comp[i, ...] = nonzero_reducer(darr.isel(time=idxs), mode)
 
-    darr_out = xr.DataArray(comp,
-                            dims=darr.dims,
-                            coords={'time': date_range.values,
-                                    'band': darr.band,
-                                    'y': darr.y,
-                                    'x': darr.x},
-                            attrs=darr.attrs)
+    darr_out = xr.DataArray(
+        comp,
+        dims=darr.dims,
+        coords={
+            "time": date_range.values,
+            "band": darr.band,
+            "y": darr.y,
+            "x": darr.x,
+        },
+        attrs=darr.attrs,
+    )
 
     return darr_out
 
 
-def _get_invervals_flags(date_range,
-                         time_vector,
-                         before,
-                         after,
-                         use_all_obs):
-
+def _get_invervals_flags(date_range, time_vector, before, after, use_all_obs):
     intervals_flags = []
     for i, d in enumerate(date_range):
-
         if i == len(date_range) - 1 and use_all_obs:
             tmp_after = None  # defaults to end of timeseries
         else:
             tmp_after = after
 
         idxs = interval_flag(
-            pd.to_datetime(time_vector),
-            d,
-            before=before,
-            after=tmp_after)
+            pd.to_datetime(time_vector), d, before=before, after=tmp_after
+        )
 
         intervals_flags.append(idxs)
 
@@ -203,10 +210,7 @@ def _get_before_after(window: int):
     return before, after
 
 
-def interval_flag(time_vector,
-                  date: datetime,
-                  before: int,
-                  after: int):
+def interval_flag(time_vector, date: datetime, before: int, after: int):
     """
     Returns a boolean array True where the dates in time_vector fall in an
     interval between data - before and date + after
@@ -224,5 +228,6 @@ def interval_flag(time_vector,
     if after is None:
         return time_vector >= midnight + timedelta(days=-before)
     else:
-        return ((time_vector >= midnight + timedelta(days=-before))
-                & (time_vector < midnight + timedelta(days=after + 1)))
+        return (time_vector >= midnight + timedelta(days=-before)) & (
+            time_vector < midnight + timedelta(days=after + 1)
+        )
